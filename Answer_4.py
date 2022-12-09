4de25be588beca70e457b64caafcfe93530972ce
Siddhant Singh <71932903+sid1hant@users.noreply.github.com>
2022-12-09 15:17:18 +0530

    Create Answer_4.py

---
+++ Answer_4.py
@@ -0,0 +1,20 @@
+from pyspark  import SparkContext
+from pyspark.sql import SparkSession
+sc =SparkContext()
+
+
+def new(line):
+    f= line.split("|")
+    w=int(f[1])
+    return f[0],w
+
+rdd1 = sc.textFile("/config/workspace/file_6.txt")
+header = rdd1.first()
+rdd2 = rdd1.filter(lambda row: row != header)
+rdd4= rdd2.map(new)
+rdd5=rdd4.filter(lambda x: x[1]>60)
+rdd6=rdd5.sortBy(lambda x : x[1],False)
+rdd7=rdd6.map(lambda x :x[0])
+rdd8= rdd7.collect()
+for a in rdd8:
+    print(a)
